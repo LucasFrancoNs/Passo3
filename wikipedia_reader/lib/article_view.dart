aabad145a32b01f175bf689fcbfd614f03103de9
LFN-FENIX <lucasnovaes479@gmail.com>
2026-08-03 22:08:05 +0000

    Termino da atividade articlewidget

--- wikipedia_reader/lib/article_view.dart
+++ wikipedia_reader/lib/article_view.dart
@@ -20,7 +20,86 @@ class _ArticleViewState extends State<ArticleView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Wikipedia Flutter')),
-      body: const Center(child: Text('Loading...')),
+      body: Center(
+        child: ListenableBuilder(
+          listenable: viewModel,
+          builder: (context, _) {
+            return switch ((
+              viewModel.isLoading,
+              viewModel.summary,
+              viewModel.error,
+            )) {
+              (true, _, _) => const CircularProgressIndicator(),
+              (_, _, final Exception e) => Text('Error: $e'),
+              (_, final summary?, _) => ArticlePage(
+                summary: summary,
+                nextArticleCallback: viewModel.fetchArticle,
+              ),
+              _ => const Text('Something went wrong!'),
+            };
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(
+            onPressed: nextArticleCallback,
+            child: const Text('Next random article'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          Text(summary.extract),
+        ],
+      ),
     );
   }
 }
